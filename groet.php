ebafa91e606da7119ffadbde6b39d15c4585dea2
Mohammed <mohammed.aboubaqr@gmail.com>
2019-12-07 16:06:17 +0100

    bruh

--- groet.php
+++ groet.php
@@ -1,7 +1,9 @@
 <?php
+
 echo "Hoi gebruiker \n" ;
 for ($i=0; $i < 3; $i++) {
-        $line = readline("Command: ");
+        $line = readline("opmerking: ");
         readline_add_history($line);
 }
+
 ?>
