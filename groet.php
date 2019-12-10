9a20aefdb0691d0fc89c4e866f3551fb5d869f8a
Mohammed <mohammed.aboubaqr@gmail.com>
2019-12-10 13:30:36 +0100

    bruh

--- groet.php
+++ groet.php
@@ -1,9 +1,7 @@
 <?php
 
 echo "Hoi gebruiker \n" ;
-for ($i=0; $i < 3; $i++) {
-        $line = readline("opmerking: ");
-        readline_add_history($line);
-}
+$line = readline("opmerking: ");
 
+echo "De gebruiker zegt: ". $line;
 ?>
